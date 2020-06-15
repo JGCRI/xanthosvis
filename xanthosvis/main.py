e7e3abf0f2b8bf8e6c643cc817ed0e90927eda9d
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2020-06-15 11:18:25 -0400

    Graph working, just need to show R correlation now

--- xanthosvis/main.py
+++ xanthosvis/main.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -19,53 +22,14 @@ colors = {
 
 df = pd.read_csv('include/Diagnostics_Runoff_Basin_Scale_km3peryr.csv')
 
-X = df['xanthos'].values.reshape(-1,1)
-y = df['VIC_1971-2000'].values.reshape(-1,1)
+fig = px.scatter(df, x="xanthos", y="VIC_1971-2000",  trendline="ols")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-regressor = LinearRegression()
-regressor.fit(X_train, y_train) #training the algorithm
-
-#To retrieve the intercept:
-print(regressor.intercept_)
-#For retrieving the slope:
-print(regressor.coef_)
-
-y_pred = regressor.predict(X_test)
-
-df.plot(x='xanthos', y='VIC_1971-2000', style='o')
-plt.scatter(X_test, y_test,  color='gray')
-plt.plot(X_test, y_pred, color='red', linewidth=2)
-plt.show()
-
-
-i: object
 app.layout = html.Div([
     dcc.Graph(
-        id='x-vs-v',
-        figure=dict(data=[
-            dict(
-                x=df[df['Name'] == i]['xanthos'],
-                y=df[df['Name'] == i]['VIC_1971-2000'],
-                text=df[df['Name'] == i]['Name'],
-                mode='markers',
-                opacity=0.7,
-                marker={
-                    'size': 15,
-                    'line': {'width': 0.5, 'color': 'white'}
-                },
-                name=i
-            ) for i in df.Name.unique()
-        ], layout=dict(
-            xaxis={'title': 'Xanthos'},
-            yaxis={'title': 'VIC'},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest'
-        ))
+        id='life-exp-vs-gdp',
+        figure=fig
     )
 ])
-
 if __name__ == '__main__':
     app.run_server(debug=True)
 
