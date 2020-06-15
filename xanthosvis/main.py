4ef6b496602a15f3ac3e1b0227ae0e879f552329
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2020-06-15 12:00:52 -0400

    First plot finished

--- xanthosvis/main.py
+++ xanthosvis/main.py
@@ -3,12 +3,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 import plotly.express as px
-import plotly.graph_objects as go
-
+import math
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -22,7 +19,22 @@ colors = {
 
 df = pd.read_csv('include/Diagnostics_Runoff_Basin_Scale_km3peryr.csv')
 
-fig = px.scatter(df, x="xanthos", y="VIC_1971-2000",  trendline="ols")
+x = df['xanthos'].values.reshape(-1,1)
+y = df['VIC_1971-2000'].values.reshape(-1,1)
+model = LinearRegression().fit(x, y)
+r_sq = model.score(x, y)
+
+fig = px.scatter(df, x="xanthos", y="VIC_1971-2000", trendline="ols")
+fig.update_layout(
+    title={'text': "Xanthos to VIC Runoff: R=" + str(round(math.sqrt(r_sq), 6)),'y':0.9, 'x':0.5,'xanchor': 'center', 'yanchor': 'top'},
+    xaxis_title="Xanthos Runoff (km\N{SUPERSCRIPT THREE}/yr)",
+    yaxis_title="VIC Runoff (km\N{SUPERSCRIPT THREE}/yr)"
+    # font=dict(
+    #     family="Courier New, monospace",
+    #     size=18,
+    #     color="#7f7f7f"
+   # )
+)
 
 app.layout = html.Div([
     dcc.Graph(
