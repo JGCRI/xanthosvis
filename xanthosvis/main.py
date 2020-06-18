622b9d239ca002e2d9405fd4f2f8ff67d9a2c6b5
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2020-06-18 09:56:44 -0400

    Pre-reorganize commit

--- xanthosvis/main.py
+++ xanthosvis/main.py
@@ -1,47 +1,229 @@
 # -*- coding: utf-8 -*-
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import plotly.express as px
 import math
+from dash.dependencies import Input, Output, State
+from datetime import datetime
+import base64
+import datetime
+import io
+import dash_table
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import xanthosvis.util_functions as xvu
+import ipywidgets as widgets
+import json
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+root_dir = '/Users/d3y010/projects/evanoff/xanthosvis/data'
+
+# reference file to be included in the package data
+gridcell_ref_file = os.path.join(root_dir, 'reference', 'xanthos_0p5deg_landcell_reference.csv')
+
+# reference geojson file for basins
+basin_json = os.path.join(root_dir, 'reference', 'gcam_basins.geojson')
+
+# in file to be uploaded by the user
+in_file = os.path.join(root_dir, 'input', 'q_km3peryear_pm_abcd_mrtm_watch_1971_2001.csv.zip')
+# get a list of available years from file
+available_year_list = xvu.get_available_years(in_file)
+
+# get start year from user input
+start_year = widgets.Dropdown(
+    options=available_year_list,
+    value=min(available_year_list),
+    description='Start Year:',
+    disabled=False,
+)
+
+display(start_year)
+print(start_year.value)
+
+# get a list of available through years
+through_years_list = xvu.available_through_years(available_year_list, start_year.value)
+
+# get through year from user input
+through_year = widgets.Dropdown(
+    options=through_years_list,
+    value=max(through_years_list),
+    description='Through Year:',
+    disabled=False,
+)
+
+display(through_year)
+print(through_year.value)
+
+# Runoff Statistic for the Choropleth Map
+acceptable_statistics = ['mean', 'median', 'min', 'max', 'standard deviation']
+
+# get through year from user input
+statistic = widgets.Dropdown(
+    options=acceptable_statistics,
+    description='Runoff statistic:',
+    disabled=False,
+)
+
+display(statistic)
+print(statistic.value)
+
+# Process years to extract from the input file
+# list comprehension to create a target year list of strings
+target_years_list = [str(i) for i in range(start_year.value, through_year.value + 1, 1)]
+#Generate data
+# read in reference file to dataframe
+df_ref = pd.read_csv(gridcell_ref_file)
+
+# read in data provided by user
+df = xvu.prepare_data(in_file, target_years_list, df_ref)
+
+# get data frame to use for plotting the choropleth map
+df_per_basin = xvu.data_per_basin(df, statistic.value, target_years_list)
+
+# create plot
+xvu.plot_choropleth(df_per_basin, basin_json)
+
+# Generate time series plot
+# # basin id will come from the click event
+basin_id = 168
+
+# # data frame of values for the target basin
+dfx = xvu.data_per_year_basin(df, basin_id)#
+# # plot hydrograph
+xvu.plot_hydrograph(dfx, basin_id)
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
-
 df = pd.read_csv('include/Diagnostics_Runoff_Basin_Scale_km3peryr.csv')
 
-x = df['xanthos'].values.reshape(-1,1)
-y = df['VIC_1971-2000'].values.reshape(-1,1)
+x = df['xanthos'].values.reshape(-1, 1)
+y = df['VIC_1971-2000'].values.reshape(-1, 1)
 model = LinearRegression().fit(x, y)
 r_sq = model.score(x, y)
 
 fig = px.scatter(df, x="xanthos", y="VIC_1971-2000", trendline="ols")
 fig.update_layout(
-    title={'text': "Xanthos to VIC Runoff: R=" + str(round(math.sqrt(r_sq), 6)),'y':0.9, 'x':0.5,'xanchor': 'center', 'yanchor': 'top'},
+    title={'text': "Xanthos to VIC Runoff: R=" + str(round(math.sqrt(r_sq), 6)), 'y': 0.9, 'x': 0.5,
+           'xanchor': 'center', 'yanchor': 'top'},
     xaxis_title="Xanthos Runoff (km\N{SUPERSCRIPT THREE}/yr)",
-    yaxis_title="VIC Runoff (km\N{SUPERSCRIPT THREE}/yr)"
+    yaxis_title="VIC Runoff (km\N{SUPERSCRIPT THREE}/yr)",
+    # height:400,
+    # margin: {'l': 10, 'b': 20, 't': 0, 'r': 0}
     # font=dict(
     #     family="Courier New, monospace",
     #     size=18,
     #     color="#7f7f7f"
-   # )
+    # ) 'height': 400,
+    #                                     'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
 )
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
+    dbc.Row(
+        [
+            dbc.Col(html.Div([
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                    },
+                    # Allow multiple files to be uploaded
+                    multiple=True
+                ),
+                html.Div(id='output-data-upload'),
+            ]), width=6, style={'border': '1px solid'}),
+            dbc.Col(
+                [
+                    dbc.Row(
+                        dbc.Col(html.Div('Nested Row1-Col2-Row1'), style={'border': '1px solid'})
+                    ),
+                    dbc.Row(
+                        dbc.Col(html.Div([
+                            dcc.Graph(
+                                id='life-exp-vs-gdp',
+                                figure=fig
+                            )
+                        ]), style={'border': '1px solid'})
+                    )
+                ],
+                width=6,
+                style={'border': '1px solid'}
+            )
+        ]
     )
-])
+]
+)
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df2 = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df2 = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df2.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df2.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
 
---
+++ xanthosvis/util_functions.py
@@ -0,0 +1,221 @@
+def get_available_years(in_file, non_year_fields=['id']):
+    """Get available years from file.  Reads only the header from the file.
+
+    :params in_file:               Full path with file name and extension to the input runoff file.
+    :type in_file:                 str
+
+    :param non_year_fields:        list of non-year fields to drop from the file
+    :type non_year_fields:         list
+
+    :return:                       list of available years as integers
+
+    """
+
+    # read in only the header of the CSV
+    df = pd.read_csv(in_file, compression='infer', nrows=0)
+
+    # drop non-year fields
+    df.drop(columns=non_year_fields, inplace=True)
+
+    return [int(i) for i in df.columns]
+
+
+def available_through_years(available_year_list, start_year):
+    """Return a list of available through years that are >= the start year.
+
+    :param available_year_list:            List of available years from the input file
+    :type available_year_list:             list
+
+    :param start_year:                     start year
+    :type start_year:                      int
+
+    :return:                               list of available through years
+
+    """
+
+    return [i for i in available_year_list if i >= start_year]
+
+
+def basin_to_gridcell_dict(df_reference):
+    """Generate a dictionary of gridcell id to basin id {grid_id: basin_id}
+
+    :param df_reference:            Input data reference dataframe
+    :type df_reference:             dataframe
+
+    :return:                        dict. {grid_id: basin_id}
+
+    """
+
+    # select target fields
+    df_reference = df_reference[['grid_id', 'basin_id']]
+
+    # set index that will become dictionary key
+    df_reference.set_index('grid_id', inplace=True)
+
+    return df_reference.to_dict()['basin_id']
+
+
+def prepare_data(in_file, yr_list, df_ref):
+    """Read in data from input file and add the basin id from a reference file.
+
+    :param in_file:                 Full path with the file name and extension to the input data file.
+    :type in_file:                  str
+
+    :param yr_list:                 List of years to process
+    :type yr_list:                  list
+
+    :param df_ref:                  Reference data frame from package
+    :type df_ref:                   dataframe
+
+    :return:                        dataframe; data with basin id
+
+    """
+
+    read_cols = yr_list + ['id']
+
+    # read in data for target years from file
+    df = pd.read_csv(in_file, compression='infer', usecols=read_cols)
+
+    # get dictionary of grid id to basin id
+    grid_basin_dict = basin_to_gridcell_dict(df_ref)
+
+    # add basin id
+    df['basin_id'] = df['id'].map(grid_basin_dict)
+
+    return df
+
+
+def data_per_basin(df, statistic, yr_list):
+    """Generate a data frame representing data per basin for all years
+    represented by an input statistic.
+
+    :param df:                      Data with basin id
+    :type df:                       dataframe
+
+    :param statistic:               statistic name from user input
+    :type statistic:                str
+
+    :param yr_list:                 List of years to process
+    :type yr_list:                  list
+
+    :return:                        dataframe; grouped by basin for statistic
+
+    """
+
+    # sum data by basin by year
+    grp = df.groupby('basin_id').sum()
+
+    grp.drop(columns=['id'], inplace=True)
+
+    # calculate stat
+    if statistic == 'mean':
+        grp['q'] = grp[yr_list].mean(axis=1)
+
+    elif statistic == 'median':
+        grp['q'] = grp[yr_list].median(axis=1)
+
+    elif statistic == 'min':
+        grp['q'] = grp[yr_list].min(axis=1)
+
+    elif statistic == 'max':
+        grp['q'] = grp[yr_list].max(axis=1)
+
+    elif statistic == 'standard deviation':
+        grp['q'] = grp[yr_list].std(axis=1)
+
+    else:
+        msg = f"The statistic requested '{statistic}' is not a valid option."
+        raise ValueError(msg)
+
+    grp.drop(columns=yr_list, inplace=True)
+
+    grp.reset_index(inplace=True)
+
+    return grp
+
+
+def data_per_year_basin(df, basin_id):
+    """Generate a data frame representing the sum of the data per year for a target basin.
+
+    :param df:                      input data having data per year
+    :type df:                       dataframe
+
+    :param basin_id:                list of years to consider
+    :type basin_id:                 list
+
+    :return:                        dataframe; sum values per year for a target basin
+
+    """
+
+    # sum data by basin by year
+    grp = df.groupby('basin_id').sum()
+
+    grp.drop(columns=['id'], inplace=True)
+
+    grp.reset_index(inplace=True)
+
+    # get only target basin_id
+    dfx = grp.loc[grp['basin_id'] == basin_id].copy()
+
+    dfx.drop(columns=['basin_id'], inplace=True)
+
+    df = dfx.T.copy()
+
+    df.reset_index(inplace=True)
+
+    df.columns = ['Year', 'q']
+
+    return df
+
+
+def process_geojson(in_file):
+    """Read in geojson spatial data and add in a feature level id.
+
+    :param in_file:                 Full path with file name and extension to the input geojson file
+    :type in_file:                  str
+
+    :return:                        geojson array
+
+    """
+    with open(basin_json) as get:
+        basin_features = json.load(get)
+
+    for fx in basin_features['features']:
+        fx['id'] = fx['properties']['basin_id']
+
+    return basin_features
+
+
+def plot_choropleth(df, geojson_file):
+    """Plot interactive choropleth map for basin level statistics.
+
+    :param df:                      dataframe with basin level stats
+    :type df:                       dataframe
+
+    :param geojson_file:            geojson file with spatial data and basin id field
+
+    """
+
+    basin_features = process_geojson(geojson_file)
+
+    fig = px.choropleth(df, geojson=basin_features, locations='basin_id', color='q',
+                        color_continuous_scale="Viridis")
+
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.show()
+
+
+def plot_hydrograph(df, basin_id):
+    """Plot a hydrograph of a specific basin.
+
+    :param df:                   Input dataframe with data and basin id for a target basin
+    :type df:                    dataframe
+
+    :param basin_id:             basin id
+    :type basin_id:              int
+
+    """
+
+    fig = px.line(dfx, x='Year', y='q', title=f"Basin {basin_id} Runoff per Year")
+
+    fig.show()
